b76f8e40f58baddad96e48ef06f1063afa47459b
Milan Milic <cenga93@gmail.com>
2021-03-19 13:43:13 +0100

    configuration gulp-nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-const { src, dest, series, watch } = require('gulp'),
-  sourcemaps = require('gulp-sourcemaps'),
-  plumber = require('gulp-plumber'),
-  sass = require('gulp-sass'),
-  sassLint = require('gulp-sass-lint'),
-  rename = require('gulp-rename'),
-  browserSync = require('browser-sync').create(),
-  babel = require('gulp-babel'),
-  uglify = require('gulp-uglify');
+const { src, dest, series, watch } = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const sassLint = require('gulp-sass-lint');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const nodemon = require('gulp-nodemon');
+const del = require('del');
 
 const paths = {
   style: {
@@ -14,9 +16,13 @@ const paths = {
     dest: `public/css/`,
     watchFiles: 'src/assets/scss/**/*.scss',
   },
+  js: {
+    src: 'src/assets/js/main.js',
+    dest: 'public/js',
+    watchFiles: 'src/assets/js/**/*.js',
+  },
 };
 
-// style
 const Style = () => {
   return src(paths.style.src)
     .pipe(sourcemaps.init())
@@ -42,27 +48,39 @@ const Style = () => {
     .pipe(browserSync.stream());
 };
 
-// js compress
-const scripts = () => {
-  return src(['src/assets/js/main.js'])
+const Script = () => {
+  return src(paths.js.src)
     .pipe(
       babel({
         presets: ['@babel/preset-env'],
       })
     )
     .pipe(uglify())
-    .pipe(dest('public/js'));
+    .pipe(dest(paths.js.dest, { sourcemaps: '.' }))
+    .pipe(browserSync.stream());
+};
+
+const Clean = () => {
+  return del(['public/css', 'public/js'], { force: true });
+};
+
+const develop = (cb) => {
+  return nodemon({
+    script: 'app.js',
+    watch: ['app.js', 'src/**/*'],
+    ignore: ['gulpfile.js', 'node_modules/**', 'public/**', 'src/assets/js'],
+  }).on('restart', () => {
+    browserSync.reload();
+  });
 };
 
 // WATCHING
-const Watching = () => {
-  const root = './';
-  browserSync.init({ server: root });
-
+const Watching = (cb) => {
   watch(paths.style.watchFiles, Style);
-  watch('src/assets/js/main.js', scripts);
+  watch(paths.js.watchFiles, Script);
+  develop(cb);
 };
 
-const build = series(Style, Watching);
+const build = series(Clean, Style, Script, Watching);
 
 exports.default = build;
--- plan.md
+++ plan.md
@@ -3,6 +3,7 @@
 
 1. Create directory structure :heavy_check_mark:
 2. Gulp.js configuration <br>
-    - set src and dest folder :heavy_check_mark:<br>
-    - add scss, js tasks :heavy_check_mark:<br>
-    - gulp-nodemon install
+   - set src and dest folder :heavy_check_mark:<br>
+   - add scss, js tasks :heavy_check_mark:<br>
+   - gulp-nodemon install :heavy_check_mark:<br>
+   - 90% automatization [x]
